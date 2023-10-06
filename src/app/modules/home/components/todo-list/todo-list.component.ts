dd3f081be772f1e245a014754814bc1600b5d302
Raylan Medeiros <raylanmedeiros.rm@gmail.com>
2023-10-06 10:02:36 -0300

    delete buttons/first output

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//interface
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent {
+
+  public taskList: Array<TaskList> = [];
+
+
+  public setEmitTaskList(event: string) {
+    this.taskList.push({task: event, checked: false})
+  }
+
+  //Single Delete
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  //Delete All
+  public deleteAllTaskList() {
+    const confirm = window.confirm("Do you want to delete all?")
+
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
 
 }
